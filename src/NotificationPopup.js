b7ffc2d4db97b33a5383c776ff823711eb963cba
nicolasvienot <nvienot@gmail.com>
2022-10-05 18:01:10 +0200

    Prevent focus on Noticeable div childs

--- src/NotificationPopup.js
+++ src/NotificationPopup.js
@@ -7,6 +7,10 @@ const config = {
 }
 
 const NotificationPopup = () => {
+  const handleFocus = (e) => {
+    e.stopPropagation()
+  }
+  
   useEffect(() => {
     window.noticeable.render('widget', config.noticeable.widget, {
       selector: '#noticeable-widget',
@@ -15,7 +19,7 @@ const NotificationPopup = () => {
       window.noticeable.destroy('widget', config.noticeable.widget)
     }
   }, [])
-  return <div id="noticeable-widget" />
+  return <div onFocus={handleFocus} id="noticeable-widget"/>
 }
 
 export default NotificationPopup
